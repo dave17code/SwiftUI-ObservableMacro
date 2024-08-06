90c4d904f23bfe9a5a74c1694f90eeb6f117b370
dave17code <dave17code@gmail.com>
2024-08-06 11:02:47 +0900

    Initial Commit

---
+++ ObservableMacro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObservableMacro
+//
+//  Created by 김두운 on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObservableMacro/ObservableMacroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableMacroApp.swift
+//  ObservableMacro
+//
+//  Created by 김두운 on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservableMacroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
